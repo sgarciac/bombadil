ba118699323c955f550020a8a67f7c2ec2ee1b82
Michael Tiller <michael.tiller@gmail.com>
2017-09-15 16:08:31 -0400

    Adding some documentation and removing unused tests

--- src/AST.ts
+++ src/AST.ts
@@ -1,8 +1,29 @@
 import ct = require('chevrotain');
 
+/**
+ * The code in this file follows a general pattern.  First, I introduce a string literal
+ * to use as the "tag" in the tagged unions.  I assign that literal to a variable so that
+ * we don't actually use string literals in the code.  Forcing the use of the variable 
+ * allows us to catch potential errors.
+ * 
+ * In addition, an interface for each node type is then defined.  Generally speaking, this
+ * interface, while exported, isn't actually that useful outside this file.  But there are
+ * some cases where they are used (hence the export).  The main reason to include the
+ * interface definition is just to explicitly state the contents of the node.
+ * 
+ * Finally, I include a "constructor" function for each interface.  Since these are interfaces
+ * and not classes, we need to define our constructor function separately.  Now, a reasonable
+ * question would be "why not uses classes and a constructor defined in the class?".  The
+ * reason is because once you have a class, you can use `instanceof` on the instances and
+ * this is dangerous because classes are values and if classes and instances are imported
+ * from different packages, things will not line up and you'll get very confusing results.
+ * Using these tagged unions and interfaces avoids this but still gives a type safe way
+ * to "switch" on different node types.
+ */
+
 // Table headers. We keep the Token for reporting errors during the loading phase.
 export const tableHeader: 'tableHeader' = 'tableHeader';
-export class TomlTableHeader {
+export interface TomlTableHeader {
     type: typeof tableHeader;
     headers: string[];
     token: ct.IToken;
@@ -23,7 +44,7 @@ export function tomlTableArrayEntryHeader(headers: string[], token: ct.IToken): 
 
 // Bindings
 export const keyValue: 'keyValue' = 'keyValue';
-export class TomlKeyValue {
+export interface TomlKeyValue {
     type: typeof keyValue;
     key: string;
     value: any;
@@ -63,13 +84,19 @@ export interface TomlAtomicGeneric<T> {
     value: T;
 }
 
+// For atomic data, first we list all the node types
 export const offsetDateTime: 'offsetDateTime' = 'offsetDateTime';
 export const localDateTime: 'localDateTime' = 'localDateTime';
 export const localDate: 'localDate' = 'localDate';
 export const localTime: 'localTime' = 'localTime';
+
+// then we list a single parameterized interface for all nodes (since they all have
+// the same structure and only the type of the data changes).
 export interface TomlAtomicDateTime extends TomlAtomicGeneric<Date> {
     type: typeof offsetDateTime | typeof localDateTime | typeof localDate | typeof localTime;
 }
+
+// Now, the constructor functions.
 export function tomlAtomicOffsetDateTime(image: string, value: Date): TomlAtomicDateTime {
     return { type: offsetDateTime, image: image, value: value };
 }
@@ -115,6 +142,8 @@ export function tomlAtomicBoolean(image: string, value: boolean): TomlAtomicBool
     return { type: atomicBoolean, image: image, value: value };
 }
 
+// Now we define a few convenient union types to represent essentially different
+// grammatic productions
 export type TomlAtomicValue =
     TomlAtomicDateTime | TomlAtomicString | TomlAtomicInteger | TomlAtomicFloat | TomlAtomicBoolean;
 
